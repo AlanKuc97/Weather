b52a1a0b51550f2529e909fd78d67fe781f7f4eb
Alanas Kučinskij <alanas.kucinskij@stud.mif.vu.lt>
2020-10-07 14:33:54 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Components/header.js'
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+    </div>
+   )
+}
+
+export default App;
